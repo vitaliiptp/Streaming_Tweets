863be3c84d15341b40242c95033e90b4ba71a357
Potapenko <vitalii.potapenko@here.com>
2020-11-17 19:39:20 +0100

    Initial commit

---
+++ tweeter_streamer.py
@@ -0,0 +1,14 @@
+import tweepy
+import csv
+from tweepy.parsers import JSONParser
+import json
+import datetime
+import pandas as pd
+import twitter_credentials as keys
+import time
+
+auth = tweepy.auth.OAuthHandler(keys.API_key, keys.API_key_secret)
+auth.set_access_token(keys.Access_token, keys.Access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=False, wait_on_rate_limit_notify=True)
+
+Tweets streamed to AWS cloud and process using NLP
